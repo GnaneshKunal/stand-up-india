39f8b3face20b1a50e1aa0e416be721dce33d29e
gnanesh <gnaneshkunal@outlook.com>
2018-04-04 21:08:52 +0530

    Add sample table

--- src/components/DocView.tsx
+++ src/components/DocView.tsx
@@ -42,6 +42,120 @@ class DocView extends React.Component<IDocViewProps, {}> {
         );
     }
 
+    standupIndiaTemplate(doc: any): JSX.Element {
+        return (
+            <div>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Name of the Bank</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Name of the Borrower</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Name of the Business</td>
+                            <td>{doc["Nature of Business Activity"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Name of the Branch</td>
+                            <td>{doc["Bank"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">17</th>
+                            <td>Number of people to whom employment has been provided</td>
+                            <td>{doc["Expected Employment"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" >19</th>
+                            <td>Mobile No.</td>
+                            <td>{doc["Mobile No"]}</td>
+                        </tr>
+                        <tr>
+                           <th scope="row">20</th>
+                           <td>E-mail</td>
+                           <td>{doc["Email Id"]}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+
     showDoc(): JSX.Element {
         let doc = this.props.doc.data;
 
@@ -49,6 +163,7 @@ class DocView extends React.Component<IDocViewProps, {}> {
             return (
                 this.template(
                     <div>
+                        {this.standupIndiaTemplate(doc[0])}
                         {JSON.stringify(doc)}
                     </div>
                 )
