3d68f4c9837e5f4cdf831d47ff74735a057c2cdb
gnanesh <gnaneshkunal@outlook.com>
2018-04-06 20:59:18 +0530

    add mnemonic

--- src/components/DocView.tsx
+++ src/components/DocView.tsx
@@ -43,25 +43,47 @@ class DocView extends React.Component<IDocViewProps, {}> {
         );
     }
 
+    makeHeader(): JSX.Element {
+        return (
+            <div>
+                <center>
+                    <h1>Chittoor District Andhra Pradesh</h1>
+                    <hr />
+                </center>
+            </div>
+        );
+    }
+
     standupIndiaTemplate(doc: any): JSX.Element {
 
         return (
             <div>
+                {this.makeHeader()}
+                <center> PRAHARI INDUSTRIES Success Story </center>
+                <br />
                 <table className="table table-bordered">
                     <tbody>
                         {
                             map(doc, (k: any, v: any) => {
-                                if (k !== null)
+                                if (k !== null) {
+                                    let value = k.toString();
+                                    if (value.includes("OF")) {
+                                        let vConv = value.indexOf("/", 10);
+                                        let vNew = value.slice(vConv + 1);
+                                        value = "SUICTRMSPL" + vNew;
+                                    }
                                     return (
                                         <tr>
                                             <td>{v.toString()}</td>
-                                            <td>{k.toString()}</td>
+                                            <td>{value}</td>
                                         </tr>
-                                    );
+                                        );
+                                }
                             })
                         }
                     </tbody>
                 </table>
+                <center>Interview Done by : Microsmartware Pvt. Ltd’ s zTeam </center>
             </div>
         );
     }
