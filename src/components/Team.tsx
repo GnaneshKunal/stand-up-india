6855963b22b3ea0aa31ab9d70d0556d874c68e50
Gnanesh Kunal <gnaneshkunal@outlook.com>
2018-05-08 23:03:16 +0530

    some content

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -18,7 +18,43 @@ export default class Team extends React.Component<{}, {}> {
                                 <img className="mx-auto rounded-circle" src="https://www.dropbox.com/s/872hcavachp8l6e/david.png?raw=1" alt="" />
                                 <h4>David Brainard</h4>
                                 <p className="text-muted">Chairman and CEO of Microsmartware.Pvt. Ltd</p>
-                                <p> David Brainard’s  goal is to help you achieve your personal and business goals faster and easier than you ever imagined. He is   a Success expert and Motivational speaker addressed more than 50,000  people in  talks and seminars throughout the Chittoor District, A.P, India , Gulf countries and Europe  like Germany and As a Keynote speaker and seminar leader, he addresses more than 5, 000  people every year. He has studied,  mini researched, spoken for  20 years in the fields of economics, history, business,  English, philosophy , psychology and   Artificial Intelligence and Human Intelligence . He speaks to corporate and public audiences on the subjects of Personal and Professional Development, including the executives and staff of many of Orgnisations. His exciting talks and seminars on Leadership, Selling, Self-Esteem, Goals, Strategy, Creativity and Success Psychology bring about immediate changes and long-term results.  His speeches   brought, bringing and will bring  Microlevel changes in Communities  to create wealth for communities and Jobs for Its People.</p>
+                                <p>  David Brainard  is  a Humble worker  who  has been blasting / Eradicating poverty through   Business  by mentoring and motivating  about  Economic Support schemes, Mudraloans, Start-up India, STAND-UP INDIA , MSME  Loans and Much more.
+
+
+
+				    Mr. A.David Brainard’s
+
+				    Mission : Turning Human Liabilities into  Human Assets in the  Communities .
+
+				    Goal       :  Passionate to Make  One Million  Millionaires   by  31st  Dec-2029
+
+				    Slogan      : Create wealth for India and Jobs for Indians
+
+				    Vision       : Make India  First and Better ( Chittoor District First & Better always)
+
+				    New Religion: “Technology is Religion,   Technocrat  is Prophet  & Silicon Valley
+
+				    is the  Holy place” according to  A.David Brainard.
+
+
+
+				    “Entrepreneurships is Religion, Fortune 500  CEO are Prophets &
+
+				    Industrial Estates are Holly Places” by  David Brainard.
+
+
+
+				    “ Wealth is Religion,  Wealth creator sare Prophets  & New York
+
+				    stock exchange is Holly  Place to entrepreneurs .”
+
+                                    by A.David    Brainard.
+
+				    Agriculture is Religion, M.S. SWAMINTHAN  is  a Prophet,
+
+				    Farmers are followers of religion,  For Apple cultivation Kashmir
+
+				    is holly place / according to  Its place of product “ by                                                                                                      A.David Brainard.     David Brainard’s  goal is to help you achieve your personal and business goals faster and easier than you ever imagined. He is   a Success expert and Motivational speaker addressed more than 50,000  people in  talks and seminars throughout the Chittoor District, A.P, India , Gulf countries and Europe  like Germany and As a Keynote speaker and seminar leader, he addresses more than 5, 000  people every year. He has studied,  mini researched, spoken for  20 years in the fields of economics, history, business,  English, philosophy , psychology and   Artificial Intelligence and Human Intelligence . He speaks to corporate and public audiences on the subjects of Personal and Professional Development, including the executives and staff of many of Orgnisations. His exciting talks and seminars on Leadership, Selling, Self-Esteem, Goals, Strategy, Creativity and Success Psychology bring about immediate changes and long-term results.  His speeches   brought, bringing and will bring  Microlevel changes in Communities  to create wealth for communities and Jobs for Its People.</p>
                                 <ul className="list-inline social-buttons">
                                     <li className="list-inline-item">
                                         <a href="#">
